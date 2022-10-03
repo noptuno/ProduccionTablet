82f87af6e2bab8254978f153969180ce584bde5d
David Garcia <desarrollo.dmr@gmail.com>
2022-10-03 20:43:12 -0300

    Initial commit

---
+++ app/src/main/java/com/codekolih/producciontablet/SeleccionImprenta.java
@@ -0,0 +1,17 @@
+package com.codekolih.producciontablet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionImprenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_imprenta);
+
+
+
+    }
+}
