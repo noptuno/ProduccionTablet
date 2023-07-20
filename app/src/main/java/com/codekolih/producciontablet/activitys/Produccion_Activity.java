c89100ff36da515c10ab2b40e90738b6905398a0
david <david.garcia2388@gmail.com>
2023-07-20 13:14:27 -0300

    20/07/2023 ajustes

--- app/src/main/java/com/codekolih/producciontablet/activitys/Produccion_Activity.java
+++ app/src/main/java/com/codekolih/producciontablet/activitys/Produccion_Activity.java
@@ -707,7 +707,7 @@ public class Produccion_Activity extends OcultarTeclado implements CantidadDialo
         produccion.put("PedidoId", pedidoId);
         produccion.put("TareaId", tareaId);
         produccion.put("MetrosImpresos", produccion_actual.getMetrosImpresos());
-        produccion.put("AnchoFinalRolloYGap", produccion_actual.getAnchoFinalRollo());
+        produccion.put("AnchoFinalRolloYGap", produccion_actual.getAnchoFinalRolloYGap());
         produccion.put("CantidadPistasImpresas", produccion_actual.getCantidadPistasImpresas());
         produccion.put("CantidadTintas", produccion_actual.getCantidadTintas());
         produccion.put("AnchoBobinaUsadoCm", produccion_actual.getAnchoBobinaUsadoCm());
@@ -794,7 +794,7 @@ public class Produccion_Activity extends OcultarTeclado implements CantidadDialo
         produccion.put("PedidoId", pedidoId);
         produccion.put("TareaId", tareaId);
         produccion.put("MetrosImpresos", produccion_actual.getMetrosImpresos());
-        produccion.put("AnchoFinalRolloYGap", produccion_actual.getAnchoFinalRollo());
+        produccion.put("AnchoFinalRolloYGap", produccion_actual.getAnchoFinalRolloYGap());
         produccion.put("CantidadPistasImpresas", produccion_actual.getCantidadPistasImpresas());
         produccion.put("CantidadTintas", produccion_actual.getCantidadTintas());
         produccion.put("AnchoBobinaUsadoCm", produccion_actual.getAnchoBobinaUsadoCm());
